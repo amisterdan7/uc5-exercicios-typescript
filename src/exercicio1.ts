088ad738ae808e0240cf03aa02880507de4099c3
amisterdan7 <ricardo.amisterdanservo@gmail.com>
2026-07-20 23:56:55 -0300

    feat(ex01): instantiate mock products array

--- src/exercicio1.ts
+++ src/exercicio1.ts
@@ -11,3 +11,9 @@ export interface Produto {
     idCategoria: number;
 };
 
+export const produtos: Produto[] = [
+  { id: 1, name: "Teclado Mecânico", price: 250.0, quantity: 10, idCategoria: 1 },
+  { id: 2, name: "Mouse Gamer", price: 120.0, quantity: 15, idCategoria: 1 },
+  { id: 3, name: "Monitor 24 Polegadas", price: 850.0, quantity: 5, idCategoria: 2 },
+];
+
